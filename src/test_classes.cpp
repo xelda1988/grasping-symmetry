851dec8402a234b1ee177406433777c0fd08632f
Alexander Rietzler <alexander@c703i-ar.uibk.ac.at>
2014-04-09 13:07:49 +0200

    backup

--- src/test_classes.cpp
+++ src/test_classes.cpp
@@ -16,6 +16,11 @@ int main () {
  
   //Do Something
   
+  
+  
+  
+  
+  
   //Test symmetry.cpp
   
   //xy Layer
@@ -49,154 +54,171 @@ int main () {
   
   
   //Load a single grasp from graspIt:
-  const char* layoutpath="/home/alexander/workspace/GraspSymmetrizer/resources/graspit/faceplate_1_grasp_graspit.xml";
-  const char* savepath = "/home/alexander/workspace/GraspSymmetrizer/resources/graspit/fp_mirrored.xml";
- 
-  Grasp graspItgrasp(layoutpath);
-  //Test gripper.cpp done
-//    graspItgrasp.saveToGraspItXml(layoutpath, savepath );
-  
-  Gripper gripper("/home/alexander/workspace/GraspSymmetrizer/resources/SchunkDexHandConfig.xml");
+//   const char* layoutpath="/home/alexander/workspace/GraspSymmetrizer/resources/graspit/faceplate_1_grasp_graspit.xml";
+//   const char* savepath = "/home/alexander/workspace/GraspSymmetrizer/resources/graspit/fp_mirrored.xml";
+//  
+//   Grasp graspItgrasp(layoutpath);
+//   //Test gripper.cpp done
+// //    graspItgrasp.saveToGraspItXml(layoutpath, savepath );
+//   
+//   Gripper gripper("/home/alexander/workspace/GraspSymmetrizer/resources/SchunkDexHandConfig.xml");
+// //   gripperInstance.printGripperInfo();
+//   
+// //   GraspDatabase graspDb("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout.xml");
+// //    graspDb.printGraspDatabase();
+//   
+//   ObjectDatabase objectDb("../resources/ObjectDatabase.xml");
+// //   objectDb.printObjectDatabase();
+//   
+//   SymmetryOperation symOp(gripper, objectDb.objectDb_.at(0), graspItgrasp );
+//   //z axis
+//   symOp.computeSymmetries();
+//   GraspDatabase graspDbOut = symOp.getGraspDb();
+//   
+//    graspDbOut.printGraspDatabase();
+//    graspDbOut.saveToXml("/home/alexander/workspace/GraspSymmetrizer/resources/graspDb_gen.xml");
+// //    graspDbOut.graspDb_.at(1).saveToGraspItXml(layoutpath, savepath );
+//   /*
+//   
+//   //Test grasp.cpp 
+//   
+//   XMLDocument doc;
+//   doc.LoadFile("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout.xml");
+//   
+//   XMLElement * graspPtr = doc.FirstChildElement()->FirstChildElement("grasp");
+//   Grasp grasp(graspPtr);
+//   grasp.printGrasp();
+//   
+//   cout << "Testing grapIt loader: " << endl;
+//   
+//   Grasp grasp2("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_GraspIt_import.xml");
+//   grasp2.printGrasp();
+//   
+//   //grasp.addToXml("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout_write.xml");
+//   
+//   
+//   GraspDatabase graspDb("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout_write.xml");
+//   graspDb.printGraspDatabase();
+//   
+//   //Test gripper.cpp done
+//   
+//   Gripper gripperInstance("/home/alexander/workspace/GraspSymmetrizer/resources/SchunkDexHandConfig.xml");
 //   gripperInstance.printGripperInfo();
-  
-//   GraspDatabase graspDb("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout.xml");
-//    graspDb.printGraspDatabase();
-  
-  ObjectDatabase objectDb("../resources/ObjectDatabase.xml");
+//   
+//   ////////////////////
+//   //Test object.cpp done
+//   
+//   XMLDocument doc;
+//   doc.LoadFile("../resources/ObjectDatabase.xml");
+//   
+//   XMLElement * xeFaceplate= doc.FirstChildElement()->FirstChildElement("object");
+//   
+//   Object faceplate(xeFaceplate);
+//   
+//   faceplate.printObjectInfo();
+//   
+//   ObjectDatabase objectDb("../resources/ObjectDatabase.xml");
+//   
 //   objectDb.printObjectDatabase();
+//   
+//   //Test util done:
   
-  SymmetryOperation symOp(gripper, objectDb.objectDb_.at(0), graspItgrasp );
-  //z axis
-  symOp.computeSymmetries();
-  GraspDatabase graspDbOut = symOp.getGraspDb();
-  
-   graspDbOut.printGraspDatabase();
-   graspDbOut.saveToXml("/home/alexander/workspace/GraspSymmetrizer/resources/graspDb_gen.xml");
-//    graspDbOut.graspDb_.at(1).saveToGraspItXml(layoutpath, savepath );
-  /*
-  
-  //Test grasp.cpp 
-  
-  XMLDocument doc;
-  doc.LoadFile("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout.xml");
-  
-  XMLElement * graspPtr = doc.FirstChildElement()->FirstChildElement("grasp");
-  Grasp grasp(graspPtr);
-  grasp.printGrasp();
-  
-  cout << "Testing grapIt loader: " << endl;
-  
-  Grasp grasp2("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_GraspIt_import.xml");
-  grasp2.printGrasp();
-  
-  //grasp.addToXml("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout_write.xml");
+//   /////////////
+//   cout << "Testing util.cpp" << endl;  
+//   const char* numberstring= "1.56";
+//   const char* numberstring2= "1";
+//   float numberfloat = 0.4;
+//   int numberint = 4;
+//   
+//   cout << "String: " << CharArrayToString(numberstring) << endl;
+//   cout << "Float: " << CharArrayToFloat(numberstring2) << endl;
+//   cout << "Int: " << CharArrayToInt(numberstring) << endl;
+//   
+//   cout << "String of Float: " << FloatToString(numberfloat) << endl;
+//   cout << "String of Int: " << IntToString(numberint) << endl;
+//   /////////////
+//   
   
   
-  GraspDatabase graspDb("/home/alexander/workspace/GraspSymmetrizer/resources/GraspDatabase_base_layout_write.xml");
-  graspDb.printGraspDatabase();
+  PoseEuler poseUr5;
   
-  //Test gripper.cpp done
+  poseUr5 << 0.05648, 0.04456, 0.4365, 1.9357, 0.1020, 0.5554;
   
-  Gripper gripperInstance("/home/alexander/workspace/GraspSymmetrizer/resources/SchunkDexHandConfig.xml");
-  gripperInstance.printGripperInfo();
+  PoseQuat poseMoveIt;
   
-  ////////////////////
-  //Test object.cpp done
+  poseMoveIt << 0.511514, -0.071577, 1.48908, 0.581692, 0.0113508, -0.766832, 0.271061;
   
-  XMLDocument doc;
-  doc.LoadFile("../resources/ObjectDatabase.xml");
+  PoseMat Ur5;
+  PoseMat Moveit;
+  PoseMat Ur5_to_Moveit;
+  PoseQuat Ur5_to_Moveit_Quat;
   
-  XMLElement * xeFaceplate= doc.FirstChildElement()->FirstChildElement("object");
+  poseEulerToPoseMat(Ur5, poseUr5);
+  poseQuatToPoseMat(Moveit, poseMoveIt);
   
-  Object faceplate(xeFaceplate);
-  
-  faceplate.printObjectInfo();
-  
-  ObjectDatabase objectDb("../resources/ObjectDatabase.xml");
-  
-  objectDb.printObjectDatabase();
-  
-  //Test util done:
-  
-  /////////////
-  cout << "Testing util.cpp" << endl;  
-  const char* numberstring= "1.56";
-  const char* numberstring2= "1";
-  float numberfloat = 0.4;
-  int numberint = 4;
-  
-  cout << "String: " << CharArrayToString(numberstring) << endl;
-  cout << "Float: " << CharArrayToFloat(numberstring2) << endl;
-  cout << "Int: " << CharArrayToInt(numberstring) << endl;
-  
-  cout << "String of Float: " << FloatToString(numberfloat) << endl;
-  cout << "String of Int: " << IntToString(numberint) << endl;
-  /////////////
-  
-  PoseMat poseMat = Matrix4f::Identity();
-  poseMat(1,3) = 0.4f;
-  PoseQuat poseQuat;
-  PoseEuler poseEuler;
-  
-  cout << "PoseMatStart: \n" << poseMat << endl;
-  
-  poseMatToPoseEuler(poseEuler, poseMat);
-  poseMatToPoseQuat(poseQuat, poseMat);
-  
-  cout << "PoseEuler: " << poseEuler.transpose() << endl;
-  cout << "PoseQuat: " << poseQuat.transpose() << endl;
-  
-  poseQuatToPoseMat(poseMat, poseQuat);
-  poseQuatToPoseEuler(poseEuler, poseQuat);
-  
-  cout << "PoseMat: \n" << poseMat << endl;
-  cout << "PoseEuler: " << poseEuler.transpose() << endl;
-  
-  poseEulerToPoseMat(poseMat, poseEuler);
-  poseEulerToPoseQuat(poseQuat, poseEuler);
-  
-  cout << "PoseMat: \n" << poseMat << endl;
-  cout << "PoseQuat: " << poseQuat.transpose() << endl;
-  
-  /////////////
-  XMLDocument doc;
-  doc.LoadFile("../resources/SchunkDexHandConfig.xml");
-  
-  XMLElement * xeManip= doc.FirstChildElement()->FirstChildElement("manipulator");
-  vector<string> attNames, attNames2;
-  vector<string> attValues;
-  vector<float> attValuesFloat;
-  vector<int> attValuesInt;
-  
-  attNames.push_back("name");
-  attNames.push_back("dof");
-  attNames2.push_back("x");
-  attNames2.push_back("y");
-  attNames2.push_back("z");
-  attNames2.push_back("roll");
-  attNames2.push_back("pitch");
-  attNames2.push_back("yaw");
-  
-  getAttributeList(attValues, attNames, xeManip);
-  
-  cout << "Attribute Values" << endl;
-  for(int i = 0; i < attValues.size(); i++)
-    cout << attValues.at(i) << endl;
-  
-  XMLElement * xeGlobalPose = xeManip->FirstChildElement("global_pose");
+  Ur5_to_Moveit = Moveit * Ur5.inverse();
+  poseMatToPoseQuat (Ur5_to_Moveit_Quat,Ur5_to_Moveit );
+  cout << "Ur5_to_Moveit: \n" << Ur5_to_Moveit << endl;
+  cout << "Ur5_to_Moveit Quat xyzwxyz: \n" << Ur5_to_Moveit_Quat << endl;
   
   
-  getAttributeList(attValuesFloat, attNames2, xeGlobalPose);
-  getAttributeList(attValuesInt, attNames2, xeGlobalPose);
-  cout << "Attribute ValuesFloat" << endl;
-  for(int i = 0; i < attValuesFloat.size(); i++)
-    cout << attValuesFloat.at(i) << endl;
-  cout << "Attribute ValuesInt" << endl;
-  for(int i = 0; i < attValuesInt.size(); i++)
-    cout << attValuesInt.at(i) << endl;
-  */
-  
-  
-  cout << "Classes Succesfully Tested! - Cheers" << endl;
+//   poseMatToPoseEuler(poseEuler, poseMat);
+//   poseMatToPoseQuat(poseQuat, poseMat);
+//   
+//   cout << "PoseEuler: " << poseEuler.transpose() << endl;
+//   cout << "PoseQuat: " << poseQuat.transpose() << endl;
+//   
+//   poseQuatToPoseMat(poseMat, poseQuat);
+//   poseQuatToPoseEuler(poseEuler, poseQuat);
+//   
+//   cout << "PoseMat: \n" << poseMat << endl;
+//   cout << "PoseEuler: " << poseEuler.transpose() << endl;
+//   
+//   poseEulerToPoseMat(poseMat, poseEuler);
+//   poseEulerToPoseQuat(poseQuat, poseEuler);
+//   
+//   cout << "PoseMat: \n" << poseMat << endl;
+//   cout << "PoseQuat: " << poseQuat.transpose() << endl;
+//   
+//   /////////////
+//   XMLDocument doc;
+//   doc.LoadFile("../resources/SchunkDexHandConfig.xml");
+//   
+//   XMLElement * xeManip= doc.FirstChildElement()->FirstChildElement("manipulator");
+//   vector<string> attNames, attNames2;
+//   vector<string> attValues;
+//   vector<float> attValuesFloat;
+//   vector<int> attValuesInt;
+//   
+//   attNames.push_back("name");
+//   attNames.push_back("dof");
+//   attNames2.push_back("x");
+//   attNames2.push_back("y");
+//   attNames2.push_back("z");
+//   attNames2.push_back("roll");
+//   attNames2.push_back("pitch");
+//   attNames2.push_back("yaw");
+//   
+//   getAttributeList(attValues, attNames, xeManip);
+//   
+//   cout << "Attribute Values" << endl;
+//   for(int i = 0; i < attValues.size(); i++)
+//     cout << attValues.at(i) << endl;
+//   
+//   XMLElement * xeGlobalPose = xeManip->FirstChildElement("global_pose");
+//   
+//   
+//   getAttributeList(attValuesFloat, attNames2, xeGlobalPose);
+//   getAttributeList(attValuesInt, attNames2, xeGlobalPose);
+//   cout << "Attribute ValuesFloat" << endl;
+//   for(int i = 0; i < attValuesFloat.size(); i++)
+//     cout << attValuesFloat.at(i) << endl;
+//   cout << "Attribute ValuesInt" << endl;
+//   for(int i = 0; i < attValuesInt.size(); i++)
+//     cout << attValuesInt.at(i) << endl;
+//   */
+//   
+//   
+//   cout << "Classes Succesfully Tested! - Cheers" << endl;
   return 0;
 }
